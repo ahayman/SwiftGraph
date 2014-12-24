1d1398f66e165fd09d7ce2337d97b2b8c7f7ae4f
Aaron Hayman <aaron@flexile.co>
2014-12-24 11:17:23 -0500

    Initial Commit

---
+++ SwiftGraph/SwiftGraph.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGraph.h
+//  SwiftGraph
+//
+//  Created by Aaron Hayman on 12/24/14.
+//  Copyright (c) 2014 FlexileSoft, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftGraph.
+FOUNDATION_EXPORT double SwiftGraphVersionNumber;
+
+//! Project version string for SwiftGraph.
+FOUNDATION_EXPORT const unsigned char SwiftGraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGraph/PublicHeader.h>
+
+
